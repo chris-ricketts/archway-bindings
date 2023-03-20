917f4cb7e6c5462ffc1760f0bd936f9801586bcc
Chris Ricketts <chris-ricketts@proton.me>
2023-03-20 13:45:31 +0000

    add(bindings): testing module QoL improvements

--- packages/bindings/src/testing.rs
+++ packages/bindings/src/testing.rs
@@ -1,28 +1,53 @@
 use std::marker::PhantomData;
 
 use cosmwasm_std::testing::{MockApi, MockQuerier, MockStorage, MOCK_CONTRACT_ADDR};
-use cosmwasm_std::{Binary, Coin, ContractResult, OwnedDeps, SystemResult};
+use cosmwasm_std::{Binary, Coin, ContractResult, OwnedDeps as CwOwnedDeps, SystemResult};
 
 use crate::ArchwayQuery;
 
-pub fn mock_dependencies(
-    custom_handler: fn(&ArchwayQuery) -> ContractResult<Binary>,
-) -> OwnedDeps<MockStorage, MockApi, MockQuerier<ArchwayQuery>, ArchwayQuery> {
+pub type MockOwnedDeps = CwOwnedDeps<MockStorage, MockApi, MockQuerier<ArchwayQuery>, ArchwayQuery>;
+
+pub trait MockDepsExt {
+    fn with_archway_query_handler<F>(self, handler: F) -> Self
+    where
+        F: Fn(&ArchwayQuery) -> ContractResult<Binary> + 'static;
+}
+
+impl MockDepsExt for MockOwnedDeps {
+    fn with_archway_query_handler<F>(self, handler: F) -> Self
+    where
+        F: Fn(&ArchwayQuery) -> ContractResult<Binary> + 'static,
+    {
+        let querier = self
+            .querier
+            .with_custom_handler(move |q| SystemResult::Ok(handler(q)));
+
+        Self { querier, ..self }
+    }
+}
+
+pub fn mock_dependencies<F>(custom_handler: F) -> MockOwnedDeps
+where
+    F: Fn(&ArchwayQuery) -> ContractResult<Binary> + 'static,
+{
     mock_dependencies_with_balance(&[], custom_handler)
 }
 
-pub fn mock_dependencies_with_balance(
+pub fn mock_dependencies_with_balance<F>(
     contract_balance: &[Coin],
-    custom_handler: fn(&ArchwayQuery) -> ContractResult<Binary>,
-) -> OwnedDeps<MockStorage, MockApi, MockQuerier<ArchwayQuery>, ArchwayQuery> {
+    custom_handler: F,
+) -> MockOwnedDeps
+where
+    F: Fn(&ArchwayQuery) -> ContractResult<Binary> + 'static,
+{
     let custom_querier: MockQuerier<ArchwayQuery> =
-        MockQuerier::new(&[(MOCK_CONTRACT_ADDR, contract_balance)])
-            .with_custom_handler(move |query| SystemResult::Ok(custom_handler(query)));
+        MockQuerier::new(&[(MOCK_CONTRACT_ADDR, contract_balance)]);
 
-    OwnedDeps {
+    MockOwnedDeps {
         storage: MockStorage::default(),
         api: MockApi::default(),
         querier: custom_querier,
         custom_query_type: PhantomData,
     }
+    .with_archway_query_handler(custom_handler)
 }
